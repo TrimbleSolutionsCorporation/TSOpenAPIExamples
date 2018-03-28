b4b53b74ef4037f1d04318d42116d6330ec130d5
Carlos Herrero <carlos.herrero@trimble.com>
2018-03-28 15:06:27 +0300

    Added WPF and filter examples

---
+++ Model/Applications/WpfBeamApplication/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using Tekla.Structures.Model;
+
+namespace WpfBeamApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private Events events = new Tekla.Structures.Model.Events();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow();
+            if(new Model().GetConnectionStatus())
+            {
+                events.TeklaStructuresExit += Events_ExitEvent;
+
+                new System.Windows.Interop.WindowInteropHelper(window).Owner = Tekla.Structures.Dialog.MainWindow.Frame.Handle;
+                window.Show();
+            }
+        }
+
+        void Events_ExitEvent()
+        {
+            base.Shutdown();
+        }
+
+
+    }
+}
