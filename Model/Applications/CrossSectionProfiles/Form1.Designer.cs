e88c64866674c31d693012fd26a06612fbe88ede
Carlos Herrero <carlos.herrero@trimble.com>
2019-03-13 13:23:27 +0200

    Adding Cross Section Profiles Example

---
+++ Model/Applications/CrossSectionProfiles/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CrossSectionProfiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First, select one part in the model";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show Cross Section Points";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Check Output window in Visual Studio to see values";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 138);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Profile Cross Section Points";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Model/Applications/CrossSectionProfiles/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+using Tekla.Structures.Catalogs;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Geometry3d;
+
+namespace CrossSectionProfiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ModelObjectSelector mos = new ModelObjectSelector();
+
+            var moe = mos.GetSelectedObjects();
+
+            if (moe.GetSize() != 1) return;
+
+            moe.MoveNext();
+            var mySelectedPart = moe.Current as Tekla.Structures.Model.Part;
+
+            if (mySelectedPart == null || String.IsNullOrEmpty(mySelectedPart.Profile.ProfileString)) return;
+
+            CrossSection section = new CrossSection(mySelectedPart.Profile.ProfileString);
+
+            if (section.Select())
+            {
+                var outerSurface = section.OuterSurface;
+                if (outerSurface != null)
+                {
+                    foreach (CrossSectionPoint point in outerSurface)
+                    {
+                        Console.WriteLine("Point " + string.Format("({0:0.000}, {1:0.000}, {2:0.000})", point.X, point.Y, point.Z));
+                        Console.WriteLine("Chamfer type " + point.Chamfer.Type.ToString());
+                        Console.WriteLine("Chamfer " + string.Format("({0:0.000}, {1:0.000}, {2:0.000}, {3:0.000})", point.Chamfer.X, point.Chamfer.Y, point.Chamfer.DZ1, point.Chamfer.DZ2));
+                    }
+
+                    Console.WriteLine(mySelectedPart.Profile.ProfileString + " cross section points selected successfully");
+                }
+
+                var outerSurfacePoints = section.OuterSurfacePoints;
+
+                if (outerSurfacePoints != null)
+                {
+                    foreach (Point p in outerSurfacePoints)
+                    {
+                        Console.WriteLine("Point " + string.Format("({0:0.000}, {1:0.000}, {2:0.000})", p.X, p.Y, p.Z));
+                    }
+
+                    Console.WriteLine(mySelectedPart.Profile.ProfileString + " cross section points selected successfully");
+                }
+            }
+        }
+    }
+}
