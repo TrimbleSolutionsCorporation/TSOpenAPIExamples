a94c957468088cbae24db56348cc039b8f5f5461
cahe <carlos.herrero@trimble.com>
2021-04-27 17:39:09 +0300

    Adding drawing weld and weld mark example

---
+++ Drawings/Applications/WeldMarkExample/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WeldMarkExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonReady = new System.Windows.Forms.Button();
+            this.buttonCreatesPlatesWeld = new System.Windows.Forms.Button();
+            this.buttonCreatesAssDrawing = new System.Windows.Forms.Button();
+            this.buttonGetWeldInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(505, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Create empty model in TeklaStructures";
+            // 
+            // buttonReady
+            // 
+            this.buttonReady.Location = new System.Drawing.Point(701, 51);
+            this.buttonReady.Name = "buttonReady";
+            this.buttonReady.Size = new System.Drawing.Size(176, 54);
+            this.buttonReady.TabIndex = 1;
+            this.buttonReady.Text = "Ready!";
+            this.buttonReady.UseVisualStyleBackColor = true;
+            this.buttonReady.Click += new System.EventHandler(this.buttonReady_Click);
+            // 
+            // buttonCreatesPlatesWeld
+            // 
+            this.buttonCreatesPlatesWeld.Enabled = false;
+            this.buttonCreatesPlatesWeld.Location = new System.Drawing.Point(80, 168);
+            this.buttonCreatesPlatesWeld.Name = "buttonCreatesPlatesWeld";
+            this.buttonCreatesPlatesWeld.Size = new System.Drawing.Size(490, 50);
+            this.buttonCreatesPlatesWeld.TabIndex = 2;
+            this.buttonCreatesPlatesWeld.Text = "Creates plates and weld in model";
+            this.buttonCreatesPlatesWeld.UseVisualStyleBackColor = true;
+            this.buttonCreatesPlatesWeld.Click += new System.EventHandler(this.buttonCreatesPlatesWeld_Click);
+            // 
+            // buttonCreatesAssDrawing
+            // 
+            this.buttonCreatesAssDrawing.Enabled = false;
+            this.buttonCreatesAssDrawing.Location = new System.Drawing.Point(80, 270);
+            this.buttonCreatesAssDrawing.Name = "buttonCreatesAssDrawing";
+            this.buttonCreatesAssDrawing.Size = new System.Drawing.Size(490, 61);
+            this.buttonCreatesAssDrawing.TabIndex = 3;
+            this.buttonCreatesAssDrawing.Text = "Creates assembly drawing with weld";
+            this.buttonCreatesAssDrawing.UseVisualStyleBackColor = true;
+            this.buttonCreatesAssDrawing.Click += new System.EventHandler(this.buttonCreatesAssDrawing_Click);
+            // 
+            // buttonGetWeldInfo
+            // 
+            this.buttonGetWeldInfo.Enabled = false;
+            this.buttonGetWeldInfo.Location = new System.Drawing.Point(80, 365);
+            this.buttonGetWeldInfo.Name = "buttonGetWeldInfo";
+            this.buttonGetWeldInfo.Size = new System.Drawing.Size(490, 97);
+            this.buttonGetWeldInfo.TabIndex = 4;
+            this.buttonGetWeldInfo.Text = "Gets weld and weld mark info from drawing views";
+            this.buttonGetWeldInfo.UseVisualStyleBackColor = true;
+            this.buttonGetWeldInfo.Click += new System.EventHandler(this.buttonGetWeldInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 535);
+            this.Controls.Add(this.buttonGetWeldInfo);
+            this.Controls.Add(this.buttonCreatesAssDrawing);
+            this.Controls.Add(this.buttonCreatesPlatesWeld);
+            this.Controls.Add(this.buttonReady);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Weld Mark Example";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonReady;
+        private System.Windows.Forms.Button buttonCreatesPlatesWeld;
+        private System.Windows.Forms.Button buttonCreatesAssDrawing;
+        private System.Windows.Forms.Button buttonGetWeldInfo;
+    }
+}
+
---
+++ Drawings/Applications/WeldMarkExample/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System.Windows.Forms;
+using Tekla.Structures;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+
+namespace WeldMarkExample
+{
+    public partial class Form1 : Form
+    {
+        Identifier plateAssembly = new Identifier();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreatesPlatesWeld_Click(object sender, System.EventArgs e)
+        {
+            var cp1 = new ContourPlate();
+            var cp2 = new ContourPlate();
+
+            Contour contour1 = new Contour();
+            contour1.AddContourPoint(new ContourPoint(new Point(2000, 0, 0), null));
+            contour1.AddContourPoint(new ContourPoint(new Point(3000, 0, 0), null));
+            contour1.AddContourPoint(new ContourPoint(new Point(3000, 2000, 0), null));
+            contour1.AddContourPoint(new ContourPoint(new Point(2000, 2000, 0), null));
+
+            Contour contour2 = new Contour();
+            contour2.AddContourPoint(new ContourPoint(new Point(3000, 0, 0), null));
+            contour2.AddContourPoint(new ContourPoint(new Point(3000, 2000, 0), null));
+            contour2.AddContourPoint(new ContourPoint(new Point(4000, 2000, 0), null));
+            contour2.AddContourPoint(new ContourPoint(new Point(4000, 0, 0), null));
+
+            cp1.Contour = contour1;
+            cp2.Contour = contour2;
+
+            cp1.Profile.ProfileString = cp2.Profile.ProfileString = "PL100";
+
+            cp1.Insert();
+            cp2.Insert();
+
+            var weld = new Tekla.Structures.Model.Weld();
+            weld.MainObject = cp1;
+            weld.SecondaryObject = cp2;
+            weld.SizeAbove = 10;
+            weld.Insert();
+
+            // This operation can also be done calling macro that executes numbering
+            Tekla.Structures.ModelInternal.Operation.dotStartAction("FullNumbering", (string)null);
+
+            double weldLength = -1;
+
+            weld.GetReportProperty("LENGTH", ref weldLength);
+
+            if (weldLength > 4000)
+            {
+                buttonCreatesPlatesWeld.Enabled = false;
+                buttonCreatesAssDrawing.Enabled = true;
+                label1.Text = " Close Numbering dialog manually! \n Assembly drawing will be created and opens automatically.";
+                plateAssembly = cp1.GetAssembly().Identifier;
+            }
+            else
+            {
+                buttonCreatesPlatesWeld.Enabled = false;
+                label1.Text = "Oops! Something went wrong. \n Restart TeklaStructures and test again.";
+            }
+
+            new Model().CommitChanges();
+        }
+
+        private void buttonReady_Click(object sender, System.EventArgs e)
+        {
+            buttonCreatesPlatesWeld.Enabled = true;
+            label1.Text = "Two plates and one weld will be inserted in model, near globlal origin";
+            buttonReady.Hide();
+        }
+
+        private void buttonCreatesAssDrawing_Click(object sender, System.EventArgs e)
+        {
+            buttonCreatesAssDrawing.Enabled = false;
+
+            var assDrawing = new AssemblyDrawing(plateAssembly);
+            assDrawing.Insert();
+            var dh = new DrawingHandler();
+            bool result = dh.SetActiveDrawing(assDrawing);
+
+            if (result)
+            {
+                label1.Text = " You can now check the assembly drawing. \n Modify content to get different results";
+                buttonGetWeldInfo.Enabled = true;
+            }
+            else
+            {
+                label1.Text = "Oops! Something went wrong. \n Restart TeklaStructures and test again.";
+            }
+
+        }
+
+        private void buttonGetWeldInfo_Click(object sender, System.EventArgs e)
+        {
+            var dh = new DrawingHandler();
+            var doe = dh.GetActiveDrawing().GetSheet().GetAllObjects(typeof(Tekla.Structures.Drawing.Weld));
+
+            int numberWelding = doe.GetSize();
+            int numberWeldingMark = 0;
+            int numberWeldMark = 0;
+
+            var doe2 = dh.GetActiveDrawing().GetSheet().GetAllObjects(typeof(WeldMark));
+
+            while (doe2.MoveNext())
+            {
+                WeldMark weldMark = doe2.Current as WeldMark;
+
+                if (weldMark.ModelIdentifier.IsValid())
+                {
+                    numberWeldingMark++;
+                }
+                else
+                {
+                    numberWeldMark++;
+                }
+            }
+
+            if (numberWelding > 0)
+            {
+                doe.MoveNext();
+                Tekla.Structures.Drawing.Weld firstWeld = doe.Current as Tekla.Structures.Drawing.Weld;
+
+                var modelWeld = new Model().SelectModelObject(firstWeld.ModelIdentifier) as Tekla.Structures.Model.Weld;
+                double weldSize = modelWeld.SizeAbove;
+                double weldLength = 0;
+                modelWeld.GetReportProperty("LENGTH", ref weldLength);
+                MessageBox.Show("Size above : " + weldSize.ToString() + " mm. \n" +
+                    "Weld length: " + weldLength.ToString() + " mm.", "First weld information");
+            }
+
+            MessageBox.Show("Number of Weld lines (related to welds in model database): " + numberWelding.ToString() + "\n" +
+                "Number of Welding marks (related to welds in model database): " + numberWeldingMark.ToString() + "\n" +
+                "Number of Weld marks (NOT related to model welds): " + numberWeldMark.ToString(), "Drawing information");
+
+        }
+    }
+}
