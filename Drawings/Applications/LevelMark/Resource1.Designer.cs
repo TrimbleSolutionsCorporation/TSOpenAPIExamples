dc02911155af03132b9613e6c3a68fbbd89a646c
Carlos Herrero <CHerrer@EU.trimblecorp.net>
2022-08-19 16:01:10 +0300

    Update framework to 4.8

--- Drawings/Applications/LevelMark/Resource1.Designer.cs
+++ Drawings/Applications/LevelMark/Resource1.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LevelMark_2019 {
+namespace LevelMark {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace LevelMark_2019 {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resource1 {
@@ -39,7 +39,7 @@ namespace LevelMark_2019 {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LevelMark_2019.Resource1", typeof(Resource1).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LevelMark.Resource1", typeof(Resource1).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Model/Applications/ShapeCatalog/Form1.Designer.cs
+++ Model/Applications/ShapeCatalog/Form1.Designer.cs
@@ -38,9 +38,10 @@ namespace ShapeCatalog
             this.structuresExtender.SetAttributeName(this.button1, null);
             this.structuresExtender.SetAttributeTypeName(this.button1, null);
             this.structuresExtender.SetBindPropertyName(this.button1, null);
-            this.button1.Location = new System.Drawing.Point(46, 24);
+            this.button1.Location = new System.Drawing.Point(123, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 43);
+            this.button1.Size = new System.Drawing.Size(360, 103);
             this.button1.TabIndex = 0;
             this.button1.Text = "BeamButton";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,10 +53,12 @@ namespace ShapeCatalog
             this.structuresExtender.SetAttributeTypeName(this.shapeCatalog1, null);
             this.shapeCatalog1.BackColor = System.Drawing.Color.Transparent;
             this.structuresExtender.SetBindPropertyName(this.shapeCatalog1, null);
-            this.shapeCatalog1.Location = new System.Drawing.Point(154, 83);
+            this.shapeCatalog1.ButtonText = "albl_Select__";
+            this.shapeCatalog1.Location = new System.Drawing.Point(411, 198);
+            this.shapeCatalog1.Margin = new System.Windows.Forms.Padding(21, 17, 21, 17);
             this.shapeCatalog1.Name = "shapeCatalog1";
             this.shapeCatalog1.SelectedShape = "";
-            this.shapeCatalog1.Size = new System.Drawing.Size(88, 27);
+            this.shapeCatalog1.Size = new System.Drawing.Size(235, 64);
             this.shapeCatalog1.TabIndex = 1;
             this.shapeCatalog1.SelectClicked += new System.EventHandler(this.shapeCatalog1_SelectClicked);
             this.shapeCatalog1.SelectionDone += new System.EventHandler(this.shapeCatalog1_SelectionDone);
@@ -65,23 +68,24 @@ namespace ShapeCatalog
             this.structuresExtender.SetAttributeName(this.textBox1, null);
             this.structuresExtender.SetAttributeTypeName(this.textBox1, null);
             this.structuresExtender.SetBindPropertyName(this.textBox1, null);
-            this.textBox1.Location = new System.Drawing.Point(9, 83);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(24, 198);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(108, 20);
+            this.textBox1.Size = new System.Drawing.Size(281, 38);
             this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.structuresExtender.SetAttributeName(this, null);
             this.structuresExtender.SetAttributeTypeName(this, null);
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.structuresExtender.SetBindPropertyName(this, null);
-            this.ClientSize = new System.Drawing.Size(259, 156);
+            this.ClientSize = new System.Drawing.Size(779, 372);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.shapeCatalog1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Form1";
             this.Text = "ShapeCatalog";
             this.TopMost = true;
