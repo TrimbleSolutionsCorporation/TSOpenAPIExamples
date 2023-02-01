0a4f0cdbafc044961a3d9a7e2602ed4d086420c3
Carlos Herrero <carlos.herrero@trimble.com>
2023-02-01 11:07:14 +0200

    Upload Side Pane examples

---
+++ Model/Plugins/SidePaneExamples/CustomPartSidePaneFeature/CustomPartSidePaneFeature.cs
@@ -0,0 +1,24 @@
+﻿using Tekla.Structures.Plugins.PropertyPane;
+
+namespace CustomPartExamplePaneFeature
+{
+    // After being compiled, CustomPartSidePaneFeature.dll must be copied to folder: \Environments\common\extensions\features
+    public class CustomPartSidePaneFeature : PluginPropertyPaneFeatureBase
+    {
+        public CustomPartSidePaneFeature()
+        {
+            // BeamCustomPart project is also available in TeklaStructures Open API Examples solution
+            this.InitializeFeature("BeamCustomPart", "BeamCustomPart", "no_help_implemented", PropertyPaneType.CustomPart, string.Empty, false);
+            BuildTemplate();
+        }
+
+        private void BuildTemplate()
+        {
+            AddPropertyGroup("Props", "General Properties");
+            AddCatalogProperty("Props", Catalog.Profile, "Profile", "", "", "Beam Profile", "");
+
+            AddPropertyGroup("General", "General");
+            AddSingleProperty("General", Single.Double, "LengthFactor", "Length Factor: ", "");
+        }
+    }
+}
---
+++ Model/Plugins/SidePaneExamples/SpliceConnSidePaneFeature/SpliceConnSidePaneFeature.cs
@@ -0,0 +1,25 @@
+﻿using Tekla.Structures.Plugins.PropertyPane;
+
+namespace SpliceConnSidePaneFeature
+{
+    public class SpliceConnSidePaneFeature : PluginPropertyPaneFeatureBase
+    {
+        public SpliceConnSidePaneFeature()
+        {
+            //InitializeFeature("SpliceConnection", "Title", string.Empty);
+            this.InitializeFeature("SpliceConnection", "SpliceConnection", "no_help_implemented", PropertyPaneType.Connection, string.Empty, false);
+            BuildTemplate();
+        }
+
+        private void BuildTemplate()
+        {
+            AddPropertyGroup("Props", "General Properties");
+            AddSingleProperty("Props", Single.Double, "PlateLength", "Plate Length", "Not in use");
+            AddSingleProperty("Props", Single.String, "BoltStandard", "Bolt Standard", "Not in use");
+
+            AddPropertyGroup("General", "General");
+            AddSingleProperty("General", Single.Integer, "OBJECT_LOCKED", "Object locked", "1 to lock");
+            AddSingleProperty("General", Single.Integer, "group_no", "Class", "class");
+        }
+    }
+}
