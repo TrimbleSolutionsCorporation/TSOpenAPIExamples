3ffbbb1c61d85db4b6b5f8958476ca3b90277c0e
Carlos Herrero <carlos.herrero@trimble.com>
2025-01-30 11:19:56 +0200

    Add PointCloud example

---
+++ Model/Applications/PointCloudExample/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+
+namespace PointCloudExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+            this.textBox1.Text = this.openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool result = File.Exists(this.textBox1.Text);
+
+            PointCloud pointCloud = new PointCloud
+            {
+                OriginalPath = this.textBox1.Text,
+                LocationBy = Guid.Empty,
+                Scale = 1.0
+            };
+
+            pointCloud.Attach();
+
+            List<Tekla.Structures.Model.UI.View> viewList = new List<Tekla.Structures.Model.UI.View>();
+            var mve = ViewHandler.GetVisibleViews();
+            
+            while(mve.MoveNext())
+            {
+                viewList.Add(mve.Current);
+            }
+
+            pointCloud.SetVisibility(viewList, true);
+        }
+    }
+}
---
+++ Model/Applications/PointCloudExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+
+namespace PointCloudExample
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Model myModel = new Model();
+
+            if (myModel.GetConnectionStatus() == false)
+            {
+                MessageBox.Show("Tekla Structures is not running.");
+                Application.Exit();
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
