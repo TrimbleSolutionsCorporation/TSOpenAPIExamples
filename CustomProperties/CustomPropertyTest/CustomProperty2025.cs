129de8af691ebb43e6f00a241411e14980c331f5
Carlos Herrero <CHerrer@EU.trimblecorp.net>
2025-03-10 12:17:37 +0200

    Added new CustomProperty with TeklaStructures attribute

---
+++ CustomProperties/CustomPropertyTest/CustomProperty2025.cs
@@ -0,0 +1,25 @@
+﻿using Tekla.Structures;
+using Tekla.Structures.Model;
+using Tekla.Structures.CustomPropertyPlugin;
+
+namespace CustomProperty2025
+{
+    [CustomPropertyPlugin("CUSTOM.TEKLAVERSION")]
+    public class CustomProperty2025 : ICustomPropertyPlugin
+    {
+        public int GetIntegerProperty(int objectId)
+        {
+            return 2025;
+        }
+
+        public double GetDoubleProperty(int objectId)
+        {
+            return 2025.0;
+        }
+
+        public string GetStringProperty(int objectId)
+        {
+            return "TS version - 2025 Beta";
+        }
+    }
+}
