d0e4aac6555c1e0d50d281a7f457b946431855c1
Carlos Herrero <CHerrer@EU.trimblecorp.net>
2025-05-26 15:43:16 +0300

    Change project name SpliceConnectionExample

--- Model/Plugins/SpliceConnection/Properties/Resources.Designer.cs
+++ Model/Plugins/SpliceConnection/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SpliceConn.Properties {
+namespace SpliceConnectionExample.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SpliceConn.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpliceConn.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpliceConnectionExample.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
