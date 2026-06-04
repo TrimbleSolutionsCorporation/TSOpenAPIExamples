b488addfbca3b06bb75ba8607147ffcee75a36d5
Carlos Herrero <carlos.herrero@trimble.com>
2026-06-04 13:14:23 +0300

    Added SaveLoad and CreateCancel

--- Model/Applications/BeamApplication/Form1.Designer.cs
+++ Model/Applications/BeamApplication/Form1.Designer.cs
@@ -28,35 +28,91 @@ namespace BeamApplication
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.saveLoad1 = new Tekla.Structures.Dialog.UIControls.SaveLoad();
+            this.createApplyCancel1 = new Tekla.Structures.Dialog.UIControls.CreateApplyCancel();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(49, 56);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "BeamButton";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.structuresExtender.SetAttributeName(this.label1, null);
+            this.structuresExtender.SetAttributeTypeName(this.label1, null);
+            this.label1.AutoSize = true;
+            this.structuresExtender.SetBindPropertyName(this.label1, null);
+            this.label1.Location = new System.Drawing.Point(12, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Profile String:";
+            // 
+            // textBox1
+            // 
+            this.structuresExtender.SetAttributeName(this.textBox1, "Profile");
+            this.structuresExtender.SetAttributeTypeName(this.textBox1, "String");
+            this.structuresExtender.SetBindPropertyName(this.textBox1, null);
+            this.textBox1.Location = new System.Drawing.Point(108, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "HEA300";
+            // 
+            // saveLoad1
+            // 
+            this.structuresExtender.SetAttributeName(this.saveLoad1, null);
+            this.structuresExtender.SetAttributeTypeName(this.saveLoad1, null);
+            this.saveLoad1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.structuresExtender.SetBindPropertyName(this.saveLoad1, null);
+            this.saveLoad1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.saveLoad1.HelpFileType = Tekla.Structures.Dialog.UIControls.SaveLoad.HelpFileTypeEnum.General;
+            this.saveLoad1.HelpKeyword = "";
+            this.saveLoad1.HelpUrl = "";
+            this.saveLoad1.Location = new System.Drawing.Point(0, 0);
+            this.saveLoad1.Name = "saveLoad1";
+            this.saveLoad1.SaveAsText = "";
+            this.saveLoad1.Size = new System.Drawing.Size(429, 43);
+            this.saveLoad1.TabIndex = 3;
+            this.saveLoad1.UserDefinedHelpFilePath = null;
+            // 
+            // createApplyCancel1
+            // 
+            this.structuresExtender.SetAttributeName(this.createApplyCancel1, null);
+            this.structuresExtender.SetAttributeTypeName(this.createApplyCancel1, null);
+            this.structuresExtender.SetBindPropertyName(this.createApplyCancel1, null);
+            this.createApplyCancel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.createApplyCancel1.Location = new System.Drawing.Point(0, 122);
+            this.createApplyCancel1.Name = "createApplyCancel1";
+            this.createApplyCancel1.Size = new System.Drawing.Size(429, 30);
+            this.createApplyCancel1.TabIndex = 4;
+            this.createApplyCancel1.CreateClicked += new System.EventHandler(this.createApplyCancel1_CreateClicked);
+            this.createApplyCancel1.ApplyClicked += new System.EventHandler(this.createApplyCancel1_ApplyClicked);
+            this.createApplyCancel1.CancelClicked += new System.EventHandler(this.createApplyCancel1_CancelClicked);
             // 
             // Form1
             // 
+            this.structuresExtender.SetAttributeName(this, null);
+            this.structuresExtender.SetAttributeTypeName(this, null);
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(231, 156);
-            this.Controls.Add(this.button1);
+            this.structuresExtender.SetBindPropertyName(this, null);
+            this.ClientSize = new System.Drawing.Size(429, 152);
+            this.Controls.Add(this.createApplyCancel1);
+            this.Controls.Add(this.saveLoad1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "BeamApplication";
             this.TopMost = true;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Tekla.Structures.Dialog.UIControls.SaveLoad saveLoad1;
+        private Tekla.Structures.Dialog.UIControls.CreateApplyCancel createApplyCancel1;
     }
 }
 
--- Model/Applications/BeamApplication/Form1.cs
+++ Model/Applications/BeamApplication/Form1.cs
@@ -1,27 +1,48 @@
 using System;
-using System.Windows.Forms;
 using Tekla.Structures.Model;
 using TSG = Tekla.Structures.Geometry3d;
+using Tekla.Structures.Dialog;
 
 namespace BeamApplication
 {
-    public partial class Form1 : Form
+    public partial class Form1 : ApplicationFormBase
     {
         public Form1()
         {
             InitializeComponent();
+            InitializeForm();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //Use default values for plugins in models without a standard attribute file
+        protected override string LoadValuesPath(string fileName)
+        {
+            SetAttributeValue(textBox1, "HEA200");
+            string Result = base.LoadValuesPath(fileName);
+            Apply();
+            return Result;
+        }
+
+        private void createApplyCancel1_ApplyClicked(object sender, EventArgs e)
+        {
+            this.Apply();
+        }
+
+        private void createApplyCancel1_CreateClicked(object sender, EventArgs e)
         {
             Model myModel = new Model();
 
             Beam myBeam = new Beam(new TSG.Point(1000, 1000, 1000),
                                     new TSG.Point(6000, 6000, 1000));
             myBeam.Material.MaterialString = "S235JR";
-            myBeam.Profile.ProfileString = "HEA400";
+            string profileName = GetAttributeValue<string>("Profile");
+            myBeam.Profile.ProfileString = profileName;
             myBeam.Insert();
             myModel.CommitChanges();
+        }
+
+        private void createApplyCancel1_CancelClicked(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
